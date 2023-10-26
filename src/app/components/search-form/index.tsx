17033058be7b540c52728c842291a62f79ffa8ef
Krzysztof Zimnicki <krzysztofzimnicki@macbook-air-personal.home>
2023-10-26 23:30:19 +0200

    preview of mobile-search

--- src/app/components/search-form/index.tsx
+++ src/app/components/search-form/index.tsx
@@ -45,32 +45,55 @@ export function SearchForm() {
   }
 
   useEffect(() => {
-  if (window.innerWidth <= 768) {
-    setMobile(true)
-  }
-  },[]);
+    if (window.innerWidth <= 768) {
+      setMobile(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const updateScrollY = () => {
+      document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+    };
+    window.addEventListener('scroll', updateScrollY);
+    return () => {
+      window.removeEventListener('scroll', updateScrollY);
+    };
+  }, []);
 
   return (
-      <motion.div
-        initial={inputFocused ? "hidden" : "show"}
-        animate={inputFocused ? "show" : "hidden"}
-        exit="exit"
-        variants={isMobile ? varsMobile : varsDesktop}
-        className="z-30 relative lg:mx-32 mt-4 mx-4 h-[100vh] overflow-hidden fixed"
-      >
-        <form className="z-30 relative px-4 py-3 items-end rounded-full border-[1px] flex h-12">
-          <input
-            className="z-30 relative rounded grow focus:ring-0 focus:outline-0"
-            placeholder="Start searching..."
-            onChange={(e) => handleSearching(e.target.value)}
-            onFocus={(e) => {
-                e.preventDefault();
-                setInputFocused(true);
-              }}
-          ></input>
-          <SearchButton />
-        </form>
-        <SearchResults searchResults={searchResults} />
-      </motion.div>
+    <motion.div
+      initial={inputFocused ? "hidden" : "show"}
+      animate={inputFocused ? "show" : "hidden"}
+      exit="exit"
+      variants={isMobile ? varsMobile : varsDesktop}
+      className="z-30 relative lg:mx-32 mt-4 mx-4 h-[100vh] overflow-hidden fixed"
+    >
+      <form className="z-30 relative px-4 py-3 items-end rounded-full border-[1px] flex h-12">
+        <input
+          className="z-30 relative rounded grow focus:ring-0 focus:outline-0"
+          placeholder="Start searching..."
+          onChange={(e) => handleSearching(e.target.value)}
+          onFocus={() => {
+            setInputFocused(true);
+            const scrollY = window.scrollY;
+            const body = document.body;
+            body.style.position = "fixed";
+            body.style.top = `-${scrollY}px`;
+            body.style.width = "100%";
+            body.style.overflow = "hidden";
+          }}
+          onBlur={() => {
+            const body = document.body;
+            body.style.position = "";
+            body.style.top = "";
+            body.style.width = "";
+            body.style.overflow = "";
+            window.scrollTo(0, parseInt(body.style.top || "0") * -1);
+          }}
+        ></input>
+        <SearchButton />
+      </form>
+      <SearchResults searchResults={searchResults} />
+    </motion.div>
   );
 }
