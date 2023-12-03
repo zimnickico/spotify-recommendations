93a6f7c74ea0d46d468ca360f85a25390ccf2390
Krzysztof Zimnicki <krzysztofzimnicki@macbook-air-personal.home>
2023-12-03 16:16:25 +0100

    ui-v2

--- src/app/components/top-bar/index.tsx
+++ src/app/components/top-bar/index.tsx
@@ -5,7 +5,7 @@ return (
     <div
     className="w-full bg-green-200 h-8 flex">
         <p
-        className="text-sm text-center m-auto">We&aposre on Product Hunt</p>
+        className="text-sm text-center m-auto">We{`'`}re on Product Hunt</p>
     </div>
 
 )
